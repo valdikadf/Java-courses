aeaa95d2681a0f24458f216082e6989ae0a3bc25
valdikadf <khvorostaniy@gmail.com>
2016-05-22 11:03:07 +0300

    Commit with class for map

--- src/quadratic/Variable.java
+++ src/quadratic/Variable.java
@@ -8,6 +8,7 @@ public class Variable {
     private String name;
     private double value;
 
+
     // Methods for set  values of fields
     public void setIsEmpty(boolean flag) { this.isEmpty = flag; }
     public void setName(String name) { this.name = name; }
@@ -17,4 +18,6 @@ public class Variable {
     public boolean getIsEmpty() { return this.isEmpty; }
     public String getName() { return this.name; }
     public double getValue() { return this.value; }
+
+    //Methods for 
 }
